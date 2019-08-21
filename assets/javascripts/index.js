5ba473c5a021b9007f0de9e5a9dc4f668dfda4f8
Txinto <txinto@gatatac.org>
2019-08-21 15:06:39 +0100

    javascript reqtree now is working on a single javascript file

--- assets/javascripts/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-import './styles.css';
-import 'react-sortable-tree/style.css';
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
